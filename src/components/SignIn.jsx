3b2c19971cb5648d2f61ba3e141762514983d96b
Minaj <minajinamdar9@gmail.com>
2024-07-05 23:24:39 +0530

    made it responsive

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -49,12 +49,12 @@ const SignIn = () => {
     <div>
         <img className="h-[100vh] w-[100vw]" src="https://assets.nflxext.com/ffe/siteui/vlv3/ff5587c5-1052-47cf-974b-a97e3b4f0656/065df910-dec3-46ae-afa8-7ad2b52dce40/IN-en-20240506-popsignuptwoweeks-perspective_alpha_website_large.jpg"
         alt="background"></img>
-        <div className="absolute bg-black top-0 w-[100vw] h-[100%] opacity-70"></div>
+        <div className="absolute bg-black top-0 w-[100vw] h-[100vh] opacity-70"></div>
 
          <Header className="absolute top-0" />
 
 
-          <form onSubmit={(e) => e.preventDefault() } className="bg-black absolute top-[10vw] left-[34vw] text-white flex flex-col w-[30%] h-[60%] rounded justify-around align-center items-center bg-opacity-80">
+          <form onSubmit={(e) => e.preventDefault() } className="bg-black absolute top-[10vw] max-[550px]:top-[23vw] max-[425px]:left-[18vw] max-[425px]:w-[70%]  left-[34vw] text-white flex flex-col  max-[900px]:w-[60%] max-[900px]:left-[22vw]   w-[30%] h-[60%] rounded justify-around align-center items-center bg-opacity-80">
             <h1 className="text-2xl font-bold p-2">{loginState ? "Sign In" : "Sign Up"}</h1>
             {loginState ? "" : <input ref={name} className="m-2 p-2 rounded text-white w-[80%] bg-black bg-opacity-25 border border-white" type="text" placeholder="Name"/>}
             <input ref={email} className="m-2 p-2 rounded text-white w-[80%] bg-black bg-opacity-25 border border-white" type="email" placeholder="Email"/>
