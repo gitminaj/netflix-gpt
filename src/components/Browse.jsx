fc2d1d1168dfdaa46b6b94fb9f65f22dc295f5da
Minaj <minajinamdar9@gmail.com>
2024-08-06 22:08:44 +0530

    fetched movie list from tmdb

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import { useEffect } from "react";
 import Header from "./Header";
+import { API_OPTIONS } from "../utils/constant";
 
 const Browse = () => {
+  const nowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json)
+  };
+
+  useEffect(()=>{
+    nowPlayingMovies();
+  },[]);
+
   return (
     <div>
       <Header></Header>
 
       <p className="p-4"> coming soon be curious byy.....</p>
     </div>
-  )
-}
+  );
+};
 
 export default Browse;
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,3 +3,11 @@ export const LOGO =
 
 export const SIGNIN_BG =
   "https://assets.nflxext.com/ffe/siteui/vlv3/ff5587c5-1052-47cf-974b-a97e3b4f0656/065df910-dec3-46ae-afa8-7ad2b52dce40/IN-en-20240506-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYmUxMmM4ZWU2MGM4NTlhMWEwMzU0NmRjYmVlMDAxYyIsIm5iZiI6MTcyMjk1MDI0MS43MTQzNDMsInN1YiI6IjY2YTkxY2U2NWU0ZDZhZWNiZmRmMDdhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UybGozHewitB0TlJ5KoRezUIRNfhutAylQY-SR5E2a0'
+  }
+};
